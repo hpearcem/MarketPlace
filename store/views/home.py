3ac6d9b4c3da588b16afff54c28fd0cad4631887
hpearcem <www.rkruger@gmail.com>
2022-07-25 03:49:08 -0400

    save changes to template file

--- store/views/home.py
+++ store/views/home.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
+# from django.views.decoretors.http import require_POST, require_GET, login_reuired
 
 
+# @require_POST
 def post(request):
-    print("home")
+    post = request.POST.get('product')
+    remove = request.POST.get('remove')
+    # cart = request.session.get('cart')
+    return redirect('home')
 
-
-def get(request):
-    print("home 2")
+# @require_GET
+"""def get(request):
+    return HttpResponseRedirect(f'/store{request.get_full_path()[1:]})"""
 
 
 def store(request):
-    print("cart")
+    """The function assigns the cart, and if there is no cart it opens an empty
+    dictionary. If a category is chosen it will return all the products in the
+    category else just all the products"""
+    # cart = request.session.get('cart')
+    # if not cart:
+    #     request.session['cart'] = {}
+    products = None
+    categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Products.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Products.get_all_products()
+
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+
+    #print('you are : ', request.session.get('email'))
+    return render(request, 'index.html', data)
+
 
 
